853d112a6f20a7de6a81a75d8abf7e54aba5f155
Omar Ibrahim Hussain <moihussain@gmail.com>
2014-12-02 17:49:12 +0530

    Code Complete
    - ready for tests

--- Water Management/waterSCU/waterSCU.ino
+++ Water Management/waterSCU/waterSCU.ino
@@ -1,4 +1,3 @@
-#include <shiftReg.h>
 /*
 Sensors to Multiplexer Pin mapping configuration:
 MUX1:
@@ -53,30 +52,9 @@ TX -  RX
 RX -  TX
 
 */
+#include <shiftReg.h>
+#include <NewPing.h>
 
-// Mux Pin assigments : Enable Pins, Slave Select Pins (commonly connected to both Multiplexers)
-const unsigned short int NumOfMux = 2;
-const int MuxEnablePin[NumOfMux]={6,10};
-const int MuxLine[NumOfMux]={2,10};
-const int MuxSlaveSelectPins[3]={9,8,7};
-
-// Shift Register Pin assigments: SCL, SCK, SER, RCK
-const int ShiftRegPinSCL = 3;
-const int ShiftRegPinSCK = 4;
-const int ShiftRegPinSER = 5;
-const int ShiftRegPinRCK = 11;
-shiftReg shiftReg1;
-
-// Ultrasonic sensor(depth sensor) TRIG line
-const int UltrasonicTRIG = A1;
-
-const unsigned short int NumOfDepthSensors=4;
-const unsigned short int NumOfFlowSensors=5;
-const unsigned short int NumOfBallSensors=7;
-const unsigned short int NumOfRelays=8;
-
-enum sensors_t {ball,depth,flow,unknownS};
-enum actuators_t {relay,unknownA};
 #define ON 1
 #define OFF 0
 
@@ -90,65 +68,131 @@ enum actuators_t {relay,unknownA};
 #define invalidType 104
 #define IDforTypeNotFound 105
 
+// RESPONSE SUCCESS
+#define RESPOND_SUCCESS 201
+#define RESPOND_FAILURE 202
+
+// Mux Pin assigments : Enable Pins, Slave Select Pins (commonly connected to both Multiplexers)
+const unsigned short int NumOfMux = 2;
+const int MuxEnablePin[NumOfMux]={6,10};
+const int MuxLine[NumOfMux]={2,10};
+const int MuxSlaveSelectPins[3]={7,8,9};
+
+// Shift Register Pin assigments: SCL, SCK, SER, RCK
+const int ShiftRegPinSRCLR = 3;
+const int ShiftRegPinSRCK = 4;
+const int ShiftRegPinSER = 5;
+const int ShiftRegPinRCK = 11;
+
+const int UltrasonicTRIGpin = A1;                           // Ultrasonic sensor(depth sensor) TRIG line
+const int MaxDepth=500;                                     //Max distance input required for Ultrasonic sensor library.
+
+shiftReg shiftReg1;                                         //Shift Register instance: Relays are triggered based in Shift Register parallel output lines
+NewPing depthSensor(UltrasonicTRIGpin,MuxLine[0],MaxDepth); // One Ultrasonic sensor instance to be used for all ultrasonic sensors connected to MUX1
+
+// Defining the number of sensors and actators of each type
+const unsigned short int NumOfDepthSensors=4;
+const unsigned short int NumOfFlowSensors=5;
+const unsigned short int NumOfBallSensors=7;
+const unsigned short int NumOfRelays=8;
+
+volatile int flowSensorCountPulses=0;                       // FlowSensor interrupt count 
+int sensorIndexNow=0xFF;
+int actuatorIndexNow=0xFF;
+boolean readCommand=0;                                      // Flag to indicate if received command was a READ sensor command
+boolean actuateCommand=0;                                   // Flag to indicate if received command was a ACTUATE device command
+boolean commandComplete = false;                            // Flag to indicate if serial data received is COMPLETE or not - '\n' is the termination character
+
+enum sensors_t {ball,depth,flow,unknownS};
+enum actuators_t {relay,unknownA};
+
 typedef struct
 {
   byte         sensorID;
   sensors_t    sensorType;
-  byte         sensorValue;
+  unsigned int sensorValue;
   byte         muxID;
   byte         muxPosition;
-}gtype_Sensor_st;
+} gtype_Sensor_st;
 
 typedef struct
 {
-  byte actuatorID;
-  actuators_t actuatorType;
-  boolean actuatorValue;
-  byte muxID;
-  byte muxPosition;
-}gtype_Actuator_st;
+  byte         actuatorID;
+  actuators_t  actuatorType;
+  boolean      actuatorValue;
+  byte         muxID;
+  byte         muxPosition;
+} gtype_Actuator_st;
 
 union
 {
   struct
   {
-  int receivedCommand;
-  byte receivedID;
-  byte receivedType;
-  byte receivedValue;
-  byte receivedReserved1;
-  byte receivedReserved2;
-  byte receivedReserved3;
-  byte receivedReserved4;
-  byte receivedReserved5;
+    int    receivedCommand;
+    byte   receivedID;
+    byte   receivedType;
+    byte   receivedValue;
+    byte   receivedReserved1;
+    byte   receivedReserved2;
+    byte   receivedReserved3;
+    byte   receivedReserved4;
+    byte   receivedReserved5;
   } receiveBufferStruct;
-  byte receivedSerialCommand[10];
-}receivedDataUnion;
+  byte   receivedSerialCommand[10];
+} receivedDataUnion;
+
+union
+{
+  struct 
+  {
+    byte resultOfOperation;
+    unsigned int value;
+  } responseToPi_st;
+  byte responseMessage[3];
+} responseToPi_u;
 
 //TODO: IDs for sensors and actuation devices to be defined earlier and read using a variable or #DEFINE
 
 gtype_Sensor_st g_DepthSensor_st[NumOfDepthSensors]={{33,depth,0,1,0},{34,depth,0,1,1},{35,depth,0,1,2},{36,depth,0,1,3}};
 gtype_Sensor_st g_TankBallSensor_st[NumOfBallSensors]={{71,ball,0,2,1},{72,ball,0,2,2},{73,ball,0,2,3},{74,ball,0,2,4},{75,ball,0,2,5},{76,ball,0,2,6},{77,ball,0,2,7}};
 gtype_Sensor_st g_FlowSensor_st[NumOfFlowSensors]={{61,flow,0,1,4},{62,flow,0,1,5},{63,flow,0,1,6},{64,flow,0,1,7},{65,flow,0,2,0}};
-gtype_Sensor_st g_SensorToRead={0,unknownS,0,0,0};
 
 gtype_Actuator_st g_Relay_st[NumOfRelays]={{51,relay,OFF,0,0},{52,relay,OFF,0,0},{53,relay,OFF,0,0},{54,relay,OFF,0,0},{55,relay,OFF,0,0},{56,relay,OFF,0,0},{57,relay,OFF,0,0},{58,relay,OFF,0,0}};
+
+gtype_Sensor_st g_SensorToRead={0,unknownS,0,0,0};
 gtype_Actuator_st g_DeviceToActuate = {0,unknownA,0,0,0};
 
+void flowSensorCount()
+{
+ flowSensorCountPulses++; 
+}
 
-boolean commandComplete = false; 
+void muxEnable(byte _muxID)
+{
+  //TODO: Check MuxEnable should be LOW or HIGH
+  digitalWrite(MuxEnablePin[_muxID],LOW);
+}
 
-unsigned short int NumberofSensorReadsRequested=0;
-unsigned short int NumberofDeviceActuationsRequested=0;
-byte toSenseID;
-byte toSenseType;
-byte SensorsToRead[8][8]={};
-byte toActuateID;
-byte toActuateType;
-byte toActuateValue;
-byte DevicesToActuate[8][8]={};
-int sensorIndexNow=0xFF;
-int actuatorIndexNow=0xFF;
+void muxDisable(byte _muxID)
+{
+  //TODO: Check MuxEnable should be LOW or HIGH
+  digitalWrite(MuxEnablePin[_muxID],HIGH);
+}
+
+
+void setMuxSlaveSelectLines(byte _slaveSelectID)
+{
+  //TODO: Check if original variable is also altered while shift operations are performed
+  //TODO: Check HIGH and LOW values in Arduino.h. Is 0x01 HIGH or 0xFF?
+  //TODO: Same to be checked for boolean also. Boolean used in writing to shiftregister
+/*  digitalWrite(MuxSlaveSelectPins[2],(_slaveSelectID & ( 1 << 2 )) >> 2 );
+  digitalWrite(MuxSlaveSelectPins[1],(_slaveSelectID & ( 1 << 1 )) >> 1 );
+  digitalWrite(MuxSlaveSelectPins[0],(_slaveSelectID & ( 1 << 0 )) >> 0 );
+  */
+  digitalWrite(MuxSlaveSelectPins[2],bitRead(_slaveSelectID,2));
+  digitalWrite(MuxSlaveSelectPins[1],bitRead(_slaveSelectID,1));
+  digitalWrite(MuxSlaveSelectPins[0],bitRead(_slaveSelectID,0));
+}
 
 void setPinConfig()
 {
@@ -160,14 +204,8 @@ void setPinConfig()
   for (int i=0;i<NumOfMux;i++)
     pinMode(MuxLine[i],INPUT);  
   
-  // Shift Register Pins  
-  pinMode(ShiftRegPinSCL,OUTPUT);
-  pinMode(ShiftRegPinSCK,OUTPUT);
-  pinMode(ShiftRegPinSER,OUTPUT);
-  pinMode(ShiftRegPinRCK,OUTPUT);
-  
   // Ultrasonic TRIG Pin
-  pinMode(UltrasonicTRIG,OUTPUT);
+  pinMode(UltrasonicTRIGpin,OUTPUT);
 }
 
 void initializeActuators()
@@ -177,6 +215,7 @@ void initializeActuators()
 
 void initializeSensors()
 {
+  //initialize ultrasonic sensors
   
 }
 
@@ -195,6 +234,7 @@ boolean isCommandValid()
                                   {
                                     sensorIndexNow=i;
                                       //TODO: Check the above statement's validity/working. Or else simply save index and use type again in sense or actuate function 
+                                    readCommand=1;  
                                     return(EXIT_SUCCESS);
                                   }
                                 }
@@ -207,6 +247,7 @@ boolean isCommandValid()
                                   {
                                     sensorIndexNow=i;
                                       //TODO: Check the above statement's validity/working. Or else simply save index and use type again in sense or actuate function 
+                                    readCommand=1;                                      
                                     return(EXIT_SUCCESS);
                                   }
                                 }
@@ -220,6 +261,7 @@ boolean isCommandValid()
                                   {
                                     sensorIndexNow=i;
                                       //TODO: Check the above statement's validity/working. Or else simply save index and use type again in sense or actuate function 
+                                    readCommand=1;                                      
                                     return(EXIT_SUCCESS);
                                   }
                                 }
@@ -238,6 +280,7 @@ boolean isCommandValid()
                                     {
                                       actuatorIndexNow=i;
                                       //TODO: Check the above statement's validity/working. Or else simply save index and use type again in sense or actuate function 
+                                      actuateCommand=1;                                      
                                       return(EXIT_SUCCESS);
                                     }
                                   }
@@ -263,7 +306,6 @@ boolean actuate(byte _actuatorIndex, unsigned short int _actuatorType,byte _valu
             return(EXIT_FAILURE);
     break;
   }
-
 }
 
 boolean sense(byte _sensorIndex, int _sensorType)
@@ -271,9 +313,18 @@ boolean sense(byte _sensorIndex, int _sensorType)
    switch(_sensorType)
   {
     //TODO : check failures in sensor read operations
-    case depth:g_DepthSensor_st[_sensorIndex].sensorValue=depthofWater(g_DepthSensor_st[_sensorIndex].muxID,g_DepthSensor_st[_sensorIndex].muxPosition);break;
-    case flow:g_FlowSensor_st[_sensorIndex].sensorValue=RateofWaterFlow(g_FlowSensor_st[_sensorIndex].muxID,g_FlowSensor_st[_sensorIndex].muxPosition);break;
-    case ball:g_TankBallSensor_st[_sensorIndex].sensorValue=isTankBallPositionClosed(g_TankBallSensor_st[_sensorIndex].muxID,g_TankBallSensor_st[_sensorIndex].muxPosition);break;
+    case depth:g_DepthSensor_st[_sensorIndex].sensorValue=depthofWater(g_DepthSensor_st[_sensorIndex].muxID-1,g_DepthSensor_st[_sensorIndex].muxPosition);
+               responseToPi_u.responseToPi_st.resultOfOperation=RESPOND_SUCCESS;
+               responseToPi_u.responseToPi_st.value=g_DepthSensor_st[_sensorIndex].sensorValue;
+               break;
+    case flow:g_FlowSensor_st[_sensorIndex].sensorValue=RateofWaterFlow(g_FlowSensor_st[_sensorIndex].muxID-1,g_FlowSensor_st[_sensorIndex].muxPosition);
+              responseToPi_u.responseToPi_st.resultOfOperation=RESPOND_SUCCESS;
+              responseToPi_u.responseToPi_st.value=g_FlowSensor_st[_sensorIndex].sensorValue;
+              break;
+    case ball:g_TankBallSensor_st[_sensorIndex].sensorValue=isTankBallPositionClosed(g_TankBallSensor_st[_sensorIndex].muxID-1,g_TankBallSensor_st[_sensorIndex].muxPosition);
+              responseToPi_u.responseToPi_st.resultOfOperation=RESPOND_SUCCESS;
+              responseToPi_u.responseToPi_st.value=g_TankBallSensor_st[_sensorIndex].sensorValue;
+              break;
     default:errorHandler(invalidType);
             return(EXIT_FAILURE);
     break;
@@ -282,7 +333,8 @@ boolean sense(byte _sensorIndex, int _sensorType)
 
 void errorHandler(unsigned short int errorCode)
 {
-  
+  responseToPi_u.responseToPi_st.resultOfOperation=RESPOND_FAILURE;
+  responseToPi_u.responseToPi_st.value=errorCode;
 }
 
 boolean motorControl(byte _relayIndex, boolean OnorOFF)
@@ -292,43 +344,77 @@ boolean motorControl(byte _relayIndex, boolean OnorOFF)
   // Form Shift register data
   for (unsigned short int i=0;i<NumOfRelays;i++)
   {
-    if(g_Relay_st[i].actuatorValue == ON)
+ /*   if(g_Relay_st[i].actuatorValue == ON)
       shiftRegData = shiftRegData | i;
     else
       shiftRegData = shiftRegData & ~i;
+  */
+  bitWrite(shiftRegData,i,g_Relay_st[i].actuatorValue);
   }
   sendDataToShiftReg(shiftRegData);
   return EXIT_SUCCESS;
 }
 
-boolean sendDataToShiftReg(byte _shiftRegData)
+void sendDataToShiftReg(byte _shiftRegData)
 {
-  
+  shiftReg1.writeData(_shiftRegData);
 }
 
 boolean isTankBallPositionClosed(byte _muxID, byte _muxPosition)
 {
-  
+  boolean retVal;
+  muxEnable(_muxID);
+  setMuxSlaveSelectLines(_muxPosition);
+  retVal=digitalRead(MuxLine[_muxID]);
+  muxDisable(_muxID);
+  return retVal;
 }
 
 int RateofWaterFlow(byte _muxID, byte _muxPosition)
 {
-  
+ int litresPerHour=0; 
+ muxEnable(_muxID);
+ setMuxSlaveSelectLines(_muxPosition);
+ sei();  // set Interrupt Enable
+ delay(1000);
+ cli();  // clear Interrupt Enable
+ litresPerHour=flowSensorCountPulses*60/7.5;
+ muxDisable(_muxID);
+ return litresPerHour;
 }
 
-int depthofWater(byte _muxID, byte _muxPosition)
+unsigned int depthofWater(byte _muxID, byte _muxPosition)
 {
-  
+ unsigned int roundTripEchoTimeInUs=0,depthInCM=0;
+ muxEnable(_muxID);
+ setMuxSlaveSelectLines(_muxPosition);
+ roundTripEchoTimeInUs=depthSensor.ping_median(5);
+ depthInCM=depthSensor.convert_cm(roundTripEchoTimeInUs);
+ muxDisable(_muxID);
+ return depthInCM;
+}
+
+void sendDataToPi(byte _responseToPi[])
+{
+ for(unsigned short i=0;i<sizeof(_responseToPi);i++)
+   Serial.write(_responseToPi[i]);
+   
 }
 
 void clearAll()
 {
+  //TODO: Check if all necessary parameters are cleared
  // SensorsToRead,DevicesToActuate,receivedDataUnion,g_SensorToRead,actuatorIndexNow,sensorIndexNow
   for(int i=0;i<10;i++)
     receivedDataUnion.receivedSerialCommand[i]=0;
   commandComplete = false;
   sensorIndexNow=0xFF;
   actuatorIndexNow=0xFF;
+  readCommand=0;
+  actuateCommand=0;
+  responseToPi_u.responseToPi_st.resultOfOperation=0;
+  responseToPi_u.responseToPi_st.value=0;
+  flowSensorCountPulses=0;
 }
 
 
@@ -338,6 +424,11 @@ void setup()
   setPinConfig();
   initializeActuators();
   initializeSensors();
+  // Initialize shift register - sets pin configurations/mapping and clear shiftregister
+  shiftReg1.srinit(ShiftRegPinSER,ShiftRegPinRCK,ShiftRegPinSRCK,ShiftRegPinSRCLR);
+  // Interrupt to detect pulses from flow sensor
+  attachInterrupt(2, flowSensorCount, RISING);
+  cli();
 }
 
 void loop()
@@ -345,22 +436,15 @@ void loop()
   if(commandComplete == true)
     if (isCommandValid())
       {
-        for (unsigned short int readIndex=0;readIndex < NumberofSensorReadsRequested;readIndex++)
-          {
-            toSenseID = SensorsToRead[readIndex][0];
-            toSenseType = SensorsToRead[readIndex][1];
-            if(!sense(toSenseID,toSenseType))
-              errorHandler(senseError);
-          }
+        if(readCommand)
+          if(!sense(sensorIndexNow,receivedDataUnion.receiveBufferStruct.receivedType))
+            errorHandler(senseError);
           
-        for (unsigned short int actuateIndex=0;actuateIndex < NumberofDeviceActuationsRequested;actuateIndex++)
-          {
-            toActuateID = DevicesToActuate[actuateIndex][0];
-            toActuateType = DevicesToActuate[actuateIndex][1];
-            if(!actuate(toActuateID,toActuateType,toActuateValue))
-              errorHandler(ActuationError);
-          }
+        if(actuateCommand)
+          if(!actuate(actuatorIndexNow,receivedDataUnion.receiveBufferStruct.receivedType,receivedDataUnion.receiveBufferStruct.receivedValue))
+            errorHandler(ActuationError);
       }
+     sendDataToPi(responseToPi_u.responseMessage);
               
   clearAll();
 }
