b1402993cee7ed1c529792a8fc51683a705de3cc
Omar Ibrahim Hussain <moihussain@gmail.com>
2015-02-04 19:42:49 +0530

    Compiling Code
    1. Menu Function framework added
    2. Moved wire operations to the library
    3. Library updated with more functions

--- FingerPrintAuth/biometricAuth/biometricMasterNew/biometricMasterNew.ino
+++ FingerPrintAuth/biometricAuth/biometricMasterNew/biometricMasterNew.ino
@@ -3,15 +3,18 @@
 #include <Menu.h>
 #include <gLCD_u8glib.h>
 #include <U8glib.h>
-#include "Wire.h"
+#include <Wire.h>
+#include <stdlib.h>
 #define DEBUG 1
-#define SLAVEADDRESS 2
+
 
 extern unsigned short currentMenu;
 extern state previousState;
 extern state currentState;
 extern state newState;
 extern menu_st Menu[7];
+extern unsigned short MenuFunctionToCallIndex;
+extern unsigned int serialInputNumber;
 
 key keypressed=NOKEY;
 boolean keyPressDetected = false;
@@ -30,39 +33,17 @@ unsigned short isKeyPressed()
   else
     return 0;
 }
-void writeToSlave(byte sendCommand[] )
-{
-  //Serial.println("Sending command to Slave:");
-  
-  Wire.beginTransmission(SLAVEADDRESS); // transmit to device #4
-  unsigned short i=0;
-  while(sendCommand[i]!=NULL)
-  {
-    Serial.print(sendCommand[i]);
-    Wire.write(sendCommand[i]);        // sends five bytes
-    i++;
-  }
-  
-  delay(500);
-}
 
-void readFromSlave()
-{
-  Wire.requestFrom(SLAVEADDRESS, 10);    // request 6 bytes from slave device #2
-  //Serial.println("Received Data:");
-  while (Wire.available())   // slave may send less than requested
-  {
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
-  }
-  
-  delay(500);
-}
+
+
 void setup() {
   Serial.begin(115200);
   pinMode(17,OUTPUT);
   digitalWrite(17,HIGH);
-  Wire.begin();
+  if(!DEBUGWOWIRE)
+  {
+    Wire.begin();
+  }
   Serial.println("End of setup");
   initializeMenus();
 }
@@ -70,17 +51,14 @@ void setup() {
 
 void loop()
 {
-  byte poll[10]={0,0,0,0,0,0,0,0,0,0};
+
   //Serial.println("Beginning Menu assignments");
  
   //Serial.println("Ending Menu Assignments");
   switch (currentState)
   {
     case HOMESCREEN:Serial.println("Current State: HOMESCREEN");
-                    poll[0]=0x50;
-                    writeToSlave(poll);
-                    delay(500);
-                    readFromSlave();
+                    poll();
                     break;
     
     case MENUSCREEN:if(isKeyPressed())
@@ -109,7 +87,73 @@ void loop()
     
     case RFIDAUTH:break;
     
-    case MENUFUNCTION: break;
+    case MENUFUNCTION: 
+                      switch(MenuFunctionToCallIndex)
+                      {
+                        case 0:deleteUser();
+                               break;
+                        case 1:setTimeFunction();
+                               break;
+                        case 2://Enroll User Fingerprint 
+                               enroll(0,0);
+                               updateState(MENUSCREEN);
+                               break;
+                        case 3://Enroll User Password 
+                               enroll(0,1);
+                               updateState(MENUSCREEN);
+                               break;
+                        case 4://Enroll User RFID Card 
+                               enroll(0,2);
+                               break;
+                        case 5://Enroll Admin Fingerprint 
+                               enroll(1,0);
+                               break;
+                        case 6://Enroll Admin Password 
+                               enroll(1,1);
+                               break;
+                        case 7://Enroll Admin RFID Card 
+                               enroll(1,2);
+                               break;
+                        case 8:// Self Test
+                               selfTest();
+                               break;
+                        case 9:// Factory Reset
+                               factoryReset();
+                               break;
+                        case 10:// Set Attendance Mode
+                                setAttendance();
+                                break;
+                        case 11:// set Door open Time
+                                setOpenTime();
+                                break;
+                        case 12:setOpenTimeOut();
+                                break;
+                        case 13:setDoorSensor();
+                                break;
+                        case 14:setBellTime();
+                                break;
+                        case 15:setVerifyMode();
+                                break;
+                        case 16:infoUser();
+                                break;
+                        case 17:infoManager();
+                                break;
+                        case 18:infoFinger();
+                                break;
+                        case 19:infoDuressFP();
+                                break;
+                        case 20:infoPassword();
+                                break;
+                        case 21:infoCard();
+                                break;
+                        case 22:infoLog();
+                                break;
+                        
+                        
+                        
+                      }
+                      
+                      break;
     
     default : break;
   
@@ -119,48 +163,62 @@ void loop()
 
 }
 void serialEvent() {
-   while (Serial.available()>0) {
-      
-    char inchar=char(Serial.read());
-    
-    keyPressDetected=true;
-    if (inchar == 'd')
-      //scrolldown();
-      keypressed=SCROLLDOWN;
-    else if(inchar=='u')
-//    scrollup();
-      keypressed=SCROLLUP;
-    else if(inchar=='e')
-      keypressed=ENTER;
-    else if (inchar=='q')
-      keypressed=ESC;
-    else if(inchar=='0')
-      keypressed=ZERO;
-    else if (inchar=='1')
-      keypressed=ONE;
-    else if(inchar=='2')
-      keypressed=TWO;
-    else if (inchar=='3')
-      keypressed=THREE;
-    else if(inchar=='4')
-      keypressed=FOUR;
-    else if (inchar=='5')
-      keypressed=FIVE;
-    else if(inchar=='6')
-      keypressed=SIX;
-    else if (inchar=='7')
-      keypressed=SEVEN;
-    else if(inchar=='8')
-      keypressed=EIGHT;
-    else if (inchar=='9')
-      keypressed=NINE;
-    else if(inchar=='m')
-      keypressed=MENU;
-    else if (inchar=='p')
-      keypressed=POWER;
-    if(DEBUG)
-      Serial.println("Key press serial event function exit");
-  }
+  
+  //  if(currentState!=MENUFUNCTION)
+  //{  
+     while (Serial.available()>0) 
+     {
+        char inchar=char(Serial.read());
+        keyPressDetected=true;
+        if (inchar == 'd')
+          //scrolldown();
+          keypressed=SCROLLDOWN;
+        else if(inchar=='u')
+    //    scrollup();
+          keypressed=SCROLLUP;
+        else if(inchar=='e')
+          keypressed=ENTER;
+        else if (inchar=='q')
+          keypressed=ESC;
+        else if(inchar=='0')
+          keypressed=ZERO;
+        else if (inchar=='1')
+          keypressed=ONE;
+        else if(inchar=='2')
+          keypressed=TWO;
+        else if (inchar=='3')
+          keypressed=THREE;
+        else if(inchar=='4')
+          keypressed=FOUR;
+        else if (inchar=='5')
+          keypressed=FIVE;
+        else if(inchar=='6')
+          keypressed=SIX;
+        else if (inchar=='7')
+          keypressed=SEVEN;
+        else if(inchar=='8')
+          keypressed=EIGHT;
+        else if (inchar=='9')
+          keypressed=NINE;
+        else if(inchar=='m')
+          keypressed=MENU;
+        else if (inchar=='p')
+          keypressed=POWER;
+        if(DEBUG)
+          Serial.println("Key press serial event function exit");
+     }
+  //}
+ /* else
+  {
+    unsigned short count=0;
+    char inchar[10]="";
+    while (Serial.available()>0) 
+    {
+        inchar[count]=char(Serial.read());
+        count++;        
+    }
+    serialInputNumber=strtoul(inchar,NULL,0);
+  }*/  
 }
 
 
